93bd096ab73f27871a2d17144c3a6f9127a2596a
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-15 13:13:07 +0200

    template immagini statiche

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra
+
+// e nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e testo
+
+// Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre.
+
+
+/* STRUMENTI
+
+- array
+
+- eventListner .click
+
+- ciclo for
+
+*/
